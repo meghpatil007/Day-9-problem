4ef46f9826bedc43c4ea8f542ca1646ed1cc675c
meghpatil007 <meghpatil48@gmail.com>
2022-08-08 11:26:14 +0530

    Added: daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,13 @@
 echo "Welcome to Employee Wage Computation"
 echo
 x=$((RANDOM%2))
+wage_per_hour=20
+full_day_hours=8
 
 if((x==0))
-then echo "employee is absent"
-else echo "employee is present"
+then
+ echo "employee is absent"
+else
+ echo "employee is present"
+ echo "Daily Wage = " $((wage_per_hour*full_day_hours))
 fi
