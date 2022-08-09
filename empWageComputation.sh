745f580c5f3bafb1c8c36af02d53a239a43816ba
meghpatil007 <meghpatil48@gmail.com>
2022-08-09 09:21:01 +0530

    Added Part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,11 +3,20 @@ echo
 x=$((RANDOM%2))
 wage_per_hour=20
 full_day_hours=8
+part_time=4
+y=$((RANDOM%2))
 
 if((x==0))
-then
- echo "employee is absent"
-else
- echo "employee is present"
- echo "Daily Wage = " $((wage_per_hour*full_day_hours))
+ then
+   echo "employee is absent"
+  elif((y==0))
+   then
+      echo "employee is present full time"
+      echo "Daily Wage = " $((wage_per_hour*full_day_hours))
+   elif((y==1))
+    then
+       echo "employee is present part time"
+       echo "daily Wage = " $((wage_per_hour*part_time))
+
 fi
+
