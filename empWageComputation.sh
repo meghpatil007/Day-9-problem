250d200036e0dad8b2c1a11c66d8dbdefdd6cc3d
meghpatil007 <meghpatil48@gmail.com>
2022-08-09 09:28:27 +0530

    Solved using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,18 @@
 echo "Welcome to Employee Wage Computation"
 echo
-x=$((RANDOM%2))
+x=$((RANDOM%3))
 wage_per_hour=20
 full_day_hours=8
 part_time=4
-y=$((RANDOM%2))
 
-if((x==0))
- then
-   echo "employee is absent"
-  elif((y==0))
-   then
-      echo "employee is present full time"
-      echo "Daily Wage = " $((wage_per_hour*full_day_hours))
-   elif((y==1))
-    then
-       echo "employee is present part time"
-       echo "daily Wage = " $((wage_per_hour*part_time))
+case $x in
+	0) echo "employee is absent"
+			;;
+	1) echo "employee is present full time"
+           echo "Daily Wage = " $((wage_per_hour*full_day_hours))
+			;;
+	2) echo "employee is present part time"
+           echo "daily Wage = " $((wage_per_hour*part_time))
 
-fi
+esac
 
