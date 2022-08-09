bcf00ca82cad17b5d17610487d9f76c10b4ec396
meghpatil007 <meghpatil48@gmail.com>
2022-08-09 09:39:17 +0530

    added monthly wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,15 +4,19 @@ x=$((RANDOM%3))
 wage_per_hour=20
 full_day_hours=8
 part_time=4
+day_per_month=20
 
 case $x in
 	0) echo "employee is absent"
 			;;
 	1) echo "employee is present full time"
-           echo "Daily Wage = " $((wage_per_hour*full_day_hours))
+           a=$((wage_per_hour*full_day_hours))
+           echo "Daily Wage = "$a 
+	   echo "Monthly Wage = " $((a*day_per_month))
 			;;
 	2) echo "employee is present part time"
-           echo "daily Wage = " $((wage_per_hour*part_time))
-
+	   b=$((wage_per_hour*part_time))
+           echo "daily Wage = "$b
+	   echo "Monthly Wage = " $((b*day_per_month))
 esac
 
