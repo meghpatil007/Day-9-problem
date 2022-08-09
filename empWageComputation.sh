5bba0c366e6bed32012a9bfd9863e8f3d508ac8b
meghpatil007 <meghpatil48@gmail.com>
2022-08-09 10:29:25 +0530

    Logic: 100 hours/ 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,38 @@
 echo "Welcome to Employee Wage Computation"
 echo
-x=$((RANDOM%3))
 wage_per_hour=20
 full_day_hours=8
 part_time=4
-day_per_month=20
+day_present=0
 
-case $x in
+count=0
+tw=0
+th=0
+while((count<20 && th<100))
+do
+ count=$((count+1))
+
+ x=$((RANDOM%3))
+  case $x in
 	0) echo "employee is absent"
-			;;
+	     		;;
 	1) echo "employee is present full time"
            a=$((wage_per_hour*full_day_hours))
-           echo "Daily Wage = "$a 
-	   echo "Monthly Wage = " $((a*day_per_month))
+	   day_present=$((day_present+1))
+           echo "Todays Wage = "$a 
+	   tw=$((tw+a))
+	   th=$((th+full_day_hours))
 			;;
 	2) echo "employee is present part time"
 	   b=$((wage_per_hour*part_time))
-           echo "daily Wage = "$b
-	   echo "Monthly Wage = " $((b*day_per_month))
-esac
+	   day_present=$((day_present+1))
+           echo "Todays Wage = "$b
+	   tw=$((tw+b))
+	   th=$((th+part_time))
 
+  esac
+ echo "Total Wage =" $tw
+# echo "Day "$count
+# echo "Total Working hour = " $th
+ echo
+done
